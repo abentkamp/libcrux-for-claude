97aa1191cde6da9ec25edb3ea9fff369ca335cc2
Jonas Schneider-Bensch <jonas@cryspen.com>
2026-02-24 13:53:12 +0100

    [aes] CCM: Add more documentation to AAD accumulation

--- crates/algorithms/aes/src/aes_ccm.rs
+++ crates/algorithms/aes/src/aes_ccm.rs
@@ -150,24 +150,38 @@ macro_rules! ccm_num_keys {
                 // case.
                 debug_assert!(MSG_ENC_LEN <= USIZE_LEN);
                 debug_assert!(MSG_ENC_LEN <= AES_BLOCK_LEN);
+                debug_assert_eq!(15 - MSG_ENC_LEN, NONCE_LEN);
 
-                // First byte of initial accumulator value:
+                // Byte 0 of initial accumulator value:
+                // bit 7: `Reserved`, set to 0
+                // bit 6: `Adata`, 1 if len(AAD) > 0, 0 otherwise
+                // bits 5..=3: `(TAG_LEN - 2) / 2` encoded in three bytes
+                // bits 2..=0: `(MSG_ENC_LEN - 1)` encoded in three bytes
                 self.accumulator[0] = 64 * (!aad.is_empty() as u8)
                     + ((TAG_LEN as u8 - 2) / 2) * 8
                     + (MSG_ENC_LEN as u8)
                     - 1;
 
+                // Bytes 1..=15-MSG_ENC_LEN contain the nonce, which
+                // is set in `set_nonce`.
+
+                // Bytes 16-MSG_ENC_LEN..=15 contain the plaintext
+                // length, encoded in `MSG_ENC_LEN` bytes.
                 self.accumulator[AES_BLOCK_LEN - MSG_ENC_LEN as usize..].copy_from_slice(
                     &payload_len.to_be_bytes()[USIZE_LEN - MSG_ENC_LEN as usize..],
                 );
 
+                // Process the initial value
                 let mut st = T::new();
                 st.load_block(&self.accumulator);
                 block_cipher(&mut st, &self.aes_state.extended_key);
-
                 st.store_block(&mut self.accumulator);
 
-                // Encode AAD length
+                // The AAD is prepended with an encoding of its length
+                // before accumulating it.
+
+                // If len(AAD) == 0, nothing further is accumulated
+                // and we move on to accumulating the plaintext.
                 let aad_len = aad.len();
                 if aad_len == 0 {
                     return;
@@ -211,17 +225,30 @@ macro_rules! ccm_num_keys {
                 // length does not exceed `usize::MAX - 10`, so this
                 // addition should not overflow.
                 if aad_len + aad_len_encoding_len <= AES_BLOCK_LEN {
+                    // If len(AAD) + aad_len_encoding_len does not fill a
+                    // full block, we write out the AAD into the current
+                    // block which is implicitly padded with zeroes, and
+                    // then accumulate.
                     current_block[aad_len_encoding_len..aad_len + aad_len_encoding_len]
                         .copy_from_slice(&aad);
 
                     self.accumulate(current_block.as_slice());
                 } else {
+                    // We have to incorporate the bytes used for the
+                    // encoding of len(AAD) into the computation of
+                    // full blocks to be accumulated.
                     let full_blocks = (aad_len_encoding_len + aad_len) / AES_BLOCK_LEN;
                     let remainder = (aad_len_encoding_len + aad_len) - full_blocks * AES_BLOCK_LEN;
+
                     let initial_aad_chunk_len = AES_BLOCK_LEN - aad_len_encoding_len;
 
                     for i in 0..full_blocks {
                         if i == 0 {
+                            // The first full block contains the
+                            // encoding of len(AAD) at the beginning,
+                            // so we can only include
+                            // `initial_aad_chunk_len` bytes from the
+                            // AAD here.
                             current_block[aad_len_encoding_len..]
                                 .copy_from_slice(&aad[0..initial_aad_chunk_len]);
                         } else {
